20f0966e26bcc3538d6ef605b8d9ba743ccebdff
lxiong1 <luexionglx@gmail.com>
2020-04-10 11:17:30 -0500

    Add logic to be able to update container

--- update-climate-data-graph.sh
+++ update-climate-data-graph.sh
@@ -6,6 +6,14 @@ mkdir -p public
 gsutil cp gs://climate-data-files/climate_data_aggregate_graph.html public/index.html
 
 IMAGE_NAME=climate-data-graph
+CONTAINER_NAME=$IMAGE_NAME-container
+LIST_CONTAINERS=$(docker container ls -a)
 
 docker build -t $IMAGE_NAME .
-docker run --name $IMAGE_NAME-container -d -p 8080:80 $IMAGE_NAME
+
+if grep "$CONTAINER_NAME" <<< $LIST_CONTAINERS; then
+    docker container stop $CONTAINER_NAME
+    docker container rm $CONTAINER_NAME
+fi
+
+docker run --name $CONTAINER_NAME -d -p 8080:80 $IMAGE_NAME
