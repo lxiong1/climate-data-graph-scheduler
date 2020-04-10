86d272597004128f7e8aea6d0b4e7a2d1f45391b
lxiong1 <luexionglx@gmail.com>
2020-04-10 09:29:07 -0500

    Rename html file on copy command

--- update-climate-data-graph.sh
+++ update-climate-data-graph.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-sudo mkdir -p public
-sudo gsutil cp gs://climate-data-files/climate_data_aggregate_graph.html public/
+mkdir -p public
+gsutil cp gs://climate-data-files/climate_data_aggregate_graph.html public/index.html
 
 IMAGE_NAME=climate-data-graph
 
-sudo docker build -t $IMAGE_NAME .
-sudo docker run --name $IMAGE_NAME-container -d -p 8080:80 $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+docker run --name $IMAGE_NAME-container -d -p 8080:80 $IMAGE_NAME
