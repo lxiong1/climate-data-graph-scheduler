ecbefa55166753a00557ed3656839955ab1ecd46
lxiong1 <luexionglx@gmail.com>
2020-04-09 20:52:06 -0500

    Add to .sh script for hosting static graph site

--- update-climate-data-graph.sh
+++ update-climate-data-graph.sh
@@ -2,5 +2,10 @@
 
 set -ex
 
-mkdir -p public
-cd public
+sudo mkdir -p public
+sudo gsutil cp gs://climate-data-files/climate_data_aggregate_graph.html public/
+
+IMAGE_NAME=climate-data-graph
+
+sudo docker build -t $IMAGE_NAME .
+sudo docker run --name $IMAGE_NAME-container -d -p 8080:80 $IMAGE_NAME
